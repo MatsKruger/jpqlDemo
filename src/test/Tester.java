bf1b0ade6ad0081098b6a30d53e6ee1198886359
Mats Krüger <matskruger@gmail.com>
2016-02-26 07:30:19 +0100

    Initial commit

---
+++ src/test/Tester.java
@@ -0,0 +1,63 @@
+package test;
+
+import entity.Student;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class Tester {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+        EntityManager em = emf.createEntityManager();
+        
+        List<Student> allStudents = em.createNamedQuery("Student.findAll", Student.class).getResultList();
+        List<Student> janStudents = em.createNamedQuery("Student.findByFirstname", Student.class).setParameter("firstname", "jan").getResultList();
+        List<Student> olsenStudents = em.createNamedQuery("Student.findByLastname", Student.class).setParameter("lastname", "olsen").getResultList();
+        
+        
+        Student stud = em.createNamedQuery("Student.findById", Student.class).setParameter("id", 1).getSingleResult();
+        Long studGradeSum = em.createQuery("SELECT sum(sp.score) FROM Studypoint sp WHERE sp.studentId = :student", Long.class).setParameter("student", stud).getSingleResult();
+        
+        Long gradeSum = em.createQuery("SELECT sum(sp.score) FROM Studypoint sp", Long.class).getSingleResult();
+        
+        // SQL Statement som finder de ønskede student ids
+        //SELECT sp.STUDENT_ID, SUM(sp.score) AS totalScore FROM studypoint sp GROUP BY sp.STUDENT_ID HAVING totalScore = (SELECT MAX(spStudGroup.totalScore) FROM (SELECT SUM(sp.score) AS totalScore, sp.STUDENT_ID FROM studypoint sp GROUP BY sp.STUDENT_ID) spStudGroup);
+        // Uden score
+        //SELECT sp.STUDENT_ID FROM studypoint sp GROUP BY sp.STUDENT_ID HAVING SUM(sp.score) = (SELECT MAX(spStudGroup.totalScore) FROM (SELECT SUM(sp.score) AS totalScore, sp.STUDENT_ID FROM studypoint sp GROUP BY sp.STUDENT_ID) spStudGroup);
+        
+        // SQL som finder studerende med højeste score
+        // SELECT * FROM STUDENT s WHERE s.id in (SELECT sp.STUDENT_ID FROM studypoint sp GROUP BY sp.STUDENT_ID HAVING SUM(sp.score) = (SELECT MAX(spStudGroup.totalScore) FROM (SELECT SUM(sp.score) AS totalScore, sp.STUDENT_ID FROM studypoint sp GROUP BY sp.STUDENT_ID) spStudGroup));
+        
+        // Jeg har ingen anelse om hvordan jeg får løst denne opgave der er nok gået 3 timer nu og pas.
+        List highestGradeSum = em.createQuery("SELECT SUM(sp.score) AS totalScore FROM Studypoint sp GROUP BY sp.studentId").getResultList();
+        
+        System.out.println(highestGradeSum + "213");
+        System.out.println("Find all Students in the system:");
+        for (Student student : allStudents) {
+            System.out.println("Name: " + student.getFirstname());
+        }
+        
+        System.out.println("Find all Students in the System with the first Name jan:");
+        for (Student student : janStudents) {
+            System.out.println("Name: " + student.getFirstname());
+        }
+        
+        System.out.println("Find all Students in the system with the last name Olsen:");
+        for (Student student : olsenStudents) {
+            System.out.println("Name: " + student.getFirstname());
+        }
+        
+        System.out.println("Find the total sum of study point scores, for a student given the student id:");
+        System.out.println(studGradeSum);
+        
+        System.out.println("Find the total sum of studypoint scores, given to all students:");
+        System.out.println(gradeSum);
+        
+        System.out.println("Find those students that has the greatest total of studypoint scores:");
+        
+        
+    }
+    
+}
